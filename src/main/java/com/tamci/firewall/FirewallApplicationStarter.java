222257f4b612458fbfe1524d53e39cf0037dfd0b
jaroslaw.blak <jaroslaw.blak@gmail.com>
2020-04-20 14:48:51 +0200

    Project init

---
+++ src/main/java/com/tamci/firewall/FirewallApplicationStarter.java
@@ -0,0 +1,13 @@
+package com.tamci.firewall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirewallApplicationStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirewallApplicationStarter.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamci/firewall/FirewallApplicationStarterTests.java
@@ -0,0 +1,13 @@
+package com.tamci.firewall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirewallApplicationStarterTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
